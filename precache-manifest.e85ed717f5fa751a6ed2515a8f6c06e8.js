f5b482e1165ede0d9a0af02f9409777daa036e2f
mishraakash404 <33085320+mishraakash404@users.noreply.github.com>
2021-02-25 18:06:28 +0530

    Updates

---
+++ precache-manifest.e85ed717f5fa751a6ed2515a8f6c06e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8c433e9036ea8c4ddc",
+    "url": "/yoboshu/static/css/main.ac06906b.chunk.css"
+  },
+  {
+    "revision": "ff8c433e9036ea8c4ddc",
+    "url": "/yoboshu/static/js/main.ad6a2da8.chunk.js"
+  },
+  {
+    "revision": "adf9b506ee81832223e9",
+    "url": "/yoboshu/static/js/runtime~main.8b1c8784.js"
+  },
+  {
+    "revision": "fa10302ad42951f1ab8a",
+    "url": "/yoboshu/static/js/2.0161fe12.chunk.js"
+  },
+  {
+    "revision": "4bc5faf2d5b61f4ad03a1291cab25fb8",
+    "url": "/yoboshu/index.html"
+  }
+];
